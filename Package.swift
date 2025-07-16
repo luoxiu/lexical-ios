ad43dff049f5ee470852d303d3e1fc9212aef60c
luoxiu <luoxiu.dev@gmail.com>
2025-07-16 23:04:01 +0800

    update endpoint

--- Package.swift
+++ Package.swift
@@ -42,7 +42,7 @@ let package = Package(
   ],
   dependencies: [
     .package(url: "https://github.com/scinfu/SwiftSoup.git", from: "2.6.0"),
-    .package(url: "https://github.com/apple/swift-markdown.git", branch: "main"),
+    .package(url: "https://github.com/swiftlang/swift-markdown.git", branch: "main"),
   ],
   targets: [
     .target(
